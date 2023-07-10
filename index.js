a6cedafa9a6dadd2247bc9aad3884c8d2fd7bd7e
herzliyaaa <herzliabarangan@gmail.com>
2023-07-10 18:34:20 +0800

    Basic setup for whatsapp integration

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const qrcode = require("qrcode-terminal");
+const puppeteer = require("puppeteer");
+
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const { Client, MessageMedia, LocalAuth } = require("whatsapp-web.js");
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--unhandled-rejections=strict",
+    ],
+  },
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.initialize();
+
+app.get("/send", (req, res) => {
+  const { message } = req.body;
+
+  console.log(req.body);
+
+  try {
+    const result = client.sendMessage(groupId, message);
+    res.status(200).send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started at port ${process.env.PORT}`);
+});
